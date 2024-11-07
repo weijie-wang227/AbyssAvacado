59137b0ba97d2ffbc67702452ebd5593db3a7699
Glenn Chiang <glennchiang24@gmail.com>
2024-11-07 19:46:36 +0800

    Add singleton AudioManager

---
+++ abyss_avocado/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance { get; private set; }
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(this);
+    }
+}
